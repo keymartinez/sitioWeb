146fa563d0c9f6e4eb613a74976fcd9c87ed60c3
keymartinez <73260089+keymartinez@users.noreply.github.com>
2025-07-27 19:43:21 -0600

    Add files via upload

---
+++ rundoliadmin.bat
@@ -0,0 +1,7 @@
+@echo off
+REM Launch Dolibarr Admin center
+REM ----------------------------
+
+REM Go to admin center page
+start "iexplore.exe" http://localhost:8080/
+REM FOR EDGE start microsoft-edge:"http://localhost:8080/"
---
+++ rundolihelp.bat
@@ -0,0 +1,8 @@
+@echo off
+REM Launch Dolibarr Help center
+REM ---------------------------
+
+REM Go to help center page
+echo Please wait...
+start "iexplore.exe" http://localhost:8080/dolibarr/support/
+REM FOR EDGE start microsoft-edge:"http://localhost:8080/dolibarr/support/"
